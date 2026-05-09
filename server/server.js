792483b80dd9a58a1480b93464232925c91c6b37
Yash Vardhan <yashvardhantomar123@gmail.com>
2026-05-10 01:54:10 +0530

    fixed render port

--- server/server.js
+++ server/server.js
@@ -92,6 +92,8 @@ socket.userColor = randomColor;
 });
 
 
-server.listen(5000, function () {
-    console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, function () {
+    console.log("server running");
 });
